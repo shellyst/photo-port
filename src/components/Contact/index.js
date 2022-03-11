5a7c07766025196ae052e6512e58f82fbb2dcaf1
Michelle <mchristine.stone91@gmail.com>
2022-03-11 14:21:11 -0500

    Added basic form structure.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
   // JSX
+  // Initialize values of the state - clear input fields on the component loading.
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = formState;
   return (
     <section>
       <h1>Contact Me</h1>
       <form id="contact-form">
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" defaultValue={name} name="name" />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" defaultValue={email} name="email" />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" defaultValue={message} rows="5" />
         </div>
         <button type="submit">Submit</button>
       </form>
