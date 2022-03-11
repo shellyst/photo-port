929925d087e2f56b085826e4f4c95aa8bd26dcd2
Michelle <mchristine.stone91@gmail.com>
2022-03-11 01:04:36 -0500

    Completed the photo port.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const { categories = [], setCurrentCategory, currentCategory } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   const handleClick = (item) => {
     console.log(item);
